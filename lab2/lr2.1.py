ad74cdabdfe9b161b09ede8325606c7823aa2953
ArtemArtemenko1 <57655220+ArtemArtemenko1@users.noreply.github.com>
2019-11-13 01:50:11 +0300

    Update lr2.1.py

--- lab2/lr2.1.py
+++ lab2/lr2.1.py
@@ -1,7 +1,7 @@
 #Організація циклу за допомогою оператора while
 import re
 def check_input(user_input):
-    pattern = r"\d"
+    pattern = r"^-?\d+(.\d+)?$"
     user_input=input()
     while not re.match(pattern,user_input):
         user_input = input("Введене значення некоректне!")
