1160df55cf0c029efa561917e1a0b35d8cd15cc5
ArtemArtemenko1 <57655220+ArtemArtemenko1@users.noreply.github.com>
2019-11-13 01:49:44 +0300

    Update lr1.3.py

--- lab1/lr1.3.py
+++ lab1/lr1.3.py
@@ -2,7 +2,7 @@
 
 import re
 def check_input(user_input):
-    pattern = r"\d"
+    pattern = r"^-?\d+(.\d+)?$"
     user_input=input()
     while not re.match(pattern,user_input):
         user_input = input("Введене значення некоректне!")
